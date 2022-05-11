12773aba94079f20ee4fbfe548799fac73f3a796
syed873 <98847170+syed873@users.noreply.github.com>
2022-05-11 20:42:56 +0500

    Add files via upload

---
+++ Controllers/OrdersController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MasterCRUD.Controllers
+{
+    public class OrdersController : Controller
+    {
+        masterEntities db = new masterEntities();
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MasterCRUD.MvcApplication" Language="C#" %>
